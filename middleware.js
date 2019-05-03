0b1b4fbb83a8e1bdca1d6071c8c70a9b6208b0b2
PedroRodriguez95 <Pedro.rodriguez6@comunidad.ub.edu.at>
2019-05-02 21:17:03 -0300

    Se agregan nuevas respuestas y se modifica el middleware para que simule un retraso

--- middleware.js
+++ middleware.js
@@ -1,7 +1,14 @@
 module.exports = function (req, res, next) {
-    if (req.method === 'POST') {
-      req.method = 'GET'
-      req.query = req.body
-    }
-    next()
+    sleep(5000).then(() => {
+      if (req.method === 'POST') {
+        req.method = 'GET'
+        req.query = req.body
+      }
+      next()
+     });
+  }
+
+
+  function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
   }
